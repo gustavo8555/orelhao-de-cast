c638003d2538018c4dfdd431c9a2af51c6f8a817
gustavo8555 <gustavo8555@hotmail.com>
2023-10-13 19:44:06 -0300

    FEAT: restricts the possible file extensions for just .wav

--- fileserver/app.py
+++ fileserver/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, abort, current_app, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 # Sets file size limit to 1MB
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+# Sets the audio permited files
+app.config['UPLOAD_EXTENSIONS'] = ['.wav']
 
 @app.route('/')
 def index():
@@ -11,6 +14,10 @@ def index():
 @app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
+    filename = uploaded_file.filename
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in current_app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
         uploaded_file.save(uploaded_file.filename)
     return redirect(url_for('index'))
