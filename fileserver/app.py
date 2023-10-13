7afa41be7145670b7b61e8933daf8db8c98387fa
gustavo8555 <gustavo8555@hotmail.com>
2023-10-13 19:03:44 -0300

    FEAT: adds file size limit

--- fileserver/app.py
+++ fileserver/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+# Sets file size limit to 1MB
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
 @app.route('/')
 def index():
