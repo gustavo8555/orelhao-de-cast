61c759322e8177b9beafb5e3f6a2210dd8723ed2
gustavo8555 <gustavo8555@hotmail.com>
2023-10-15 02:51:21 -0300

    WIP: modifies record function

--- js/app.js
+++ js/app.js
@@ -116,52 +116,45 @@ function stopRecording() {
 }
 
 function createDownloadLink(blob) {
-	
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
+    var url = URL.createObjectURL(blob);
+    var au = document.createElement('audio');
+    var li = document.createElement('li');
+    var link = document.createElement('a');
 
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
+    var filename = new Date().toISOString() + ".wav"; // Include the file extension in the filename
 
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
+    au.controls = true;
+    au.src = url;
 
-	//save to disk link
-	link.href = url;
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
+    link.href = url;
+    link.download = filename;
+    link.innerHTML = "Save to disk";
 
-	//add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
+    li.appendChild(au);
+    li.appendChild(document.createTextNode(filename + " "));
 
-	//add the save to disk link to li
-	li.appendChild(link);
-	
-	//upload link
-	var upload = document.createElement('a');
-	upload.href="#";
-	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-		  var xhr=new XMLHttpRequest();
-		  xhr.onload=function(e) {
-		      if(this.readyState === 4) {
-		          console.log("Server returned: ",e.target.responseText);
-		      }
-		  };
-		  var fd=new FormData();
-		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.php",true);
-		  xhr.send(fd);
-	})
-	li.appendChild(document.createTextNode (" "))//add a space in between
-	li.appendChild(upload)//add the upload link to li
+    var record = document.createElement('a');
+    record.href = "#";
+    record.innerHTML = "Record";
+    record.addEventListener("click", function(event) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function(e) {
+            if (this.readyState === 4) {
+                console.log("Server returned: ", e.target.responseText);
+            }
+        };
+        var fd = new FormData();
+        fd.append("audio_data", blob, filename);
 
-	//add the li element to the ol
-	recordingsList.appendChild(li);
+        // Update the URL to point to your Python service's /record endpoint
+        xhr.open("POST", "/record", true);
+
+        // Send the audio data to the /record endpoint
+        xhr.send(fd);
+    });
+
+    li.appendChild(document.createTextNode(" "));
+    li.appendChild(record);
+
+    recordingsList.appendChild(li);
 }
