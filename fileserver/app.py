497772f8ce22d1bfaa756b079d5c298ffcd52625
gustavo8555 <gustavo8555@hotmail.com>
2023-10-29 16:51:37 -0300

    FEAT: saves files in the setted RECORD_FOLDER_NAME var

--- fileserver/app.py
+++ fileserver/app.py
@@ -13,21 +13,33 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 # Sets the audio permited files
 app.config['UPLOAD_EXTENSIONS'] = ['.wav']
 
+RECORD_FOLDER_NAME = 'records' 
+
 @app.route('/record', methods=['POST', 'OPTIONS'])
 def upload_file():
-    # print('REQUEST >>>>>>>>>>>>>>>>>>>>>>>>>>>' + request)
     if request.method == 'OPTIONS':
         return make_response('Preflight OK', 200, {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*"})
     else:
         uploaded_file = request.files['file']
+        # uploaded_file.filename = uploaded_file.
         filename = secure_filename(uploaded_file.filename)
+        print('filename: ' + filename)
         if filename != '':
             file_ext = os.path.splitext(filename)[1]
             if file_ext not in current_app.config['UPLOAD_EXTENSIONS']:
                 abort(400)
             # TODO: add a file name logic
-            uploaded_file.save(uploaded_file.filename)
+            uploaded_file.save(append_folder_name(uploaded_file.filename))
         return make_response('File uploaded successfully')
+
+def append_folder_name(fullpath, server_path=RECORD_FOLDER_NAME):
+    filename_path_chunks = fullpath.split('/')
+    filename = filename_path_chunks[len(filename_path_chunks)-1]
+    base_path = filename_path_chunks[0:len(filename_path_chunks)-1]
+    base_path.append(server_path)
+    base_path.append(filename)
+    return '/'.join(base_path)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=8080)
